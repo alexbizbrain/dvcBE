139d44fe0f1d16942193cee5333db5e22a236e36
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-09-19 20:14:11 +0400

    implement get all claims

--- src/user-dashboard/user-dashboard.controller.ts
+++ src/user-dashboard/user-dashboard.controller.ts
@@ -1,35 +1,16 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UserDashboardService } from './user-dashboard.service';
 import { CurrentUser } from 'src/common/auth/decorators/current-user.decorator';
-import { ClaimsQueryDto, PageQueryDto } from './dto/claims-query.dto';
-import { JwtAuthGuard } from 'src/common/auth/guards/jwt-auth.guard';
+import type { User } from '@prisma/client';
+import { GetClaimsQueryDto } from './dto/claims-query.dto';
 
-@UseGuards(JwtAuthGuard)
 @Controller('user-dashboard')
 export class UserDashboardController {
   constructor(private readonly userDashboardService: UserDashboardService) {}
 
-  @Get('summary')
-  summary(@CurrentUser() user: { id: string }) {
-    return this.userDashboardService.getSummary(user.id);
-  }
-
-  @Get('active-claim')
-  activeClaim(@CurrentUser() user: { id: string }) {
-    return this.userDashboardService.getActiveClaim(user.id);
-  }
-
   @Get('claims')
-  listClaims(
-    @CurrentUser() user: { id: string },
-    @Query() query: ClaimsQueryDto,
-  ) {
-    return this.userDashboardService.listClaims(user.id, query);
-  }
-
-  @Get('documents')
-  documents(@CurrentUser() user: { id: string }, @Query() q: PageQueryDto) {
-    const lim = Number(q?.pageSize ?? 2);
-    return this.userDashboardService.getLatestDocuments(user.id, lim);
+  async listMine(@CurrentUser() user: User, @Query() q: GetClaimsQueryDto) {
+    const userId = user.id;
+    return this.userDashboardService.listForUser(userId, q);
   }
 }
