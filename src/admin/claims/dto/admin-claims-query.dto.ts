50d8321720b5bf4ef946084f387904b152c44e7b
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-09-26 11:41:09 +0400

    implement fix

--- src/admin/claims/dto/admin-claims-query.dto.ts
+++ src/admin/claims/dto/admin-claims-query.dto.ts
@@ -120,11 +120,8 @@ export class AdminClaimsQueryDto {
   @IsInt()
   @Min(1)
   limit: number = 10;
-<<<<<<< HEAD
 
   @IsOptional()
   @IsString()
   userId?: string;
-=======
->>>>>>> 9d7d8fa37dbb0c236ac6fda491c7e754f3dbc6aa
 }
